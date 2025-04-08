2bfc28958d0a0dc46e58eab69cc88c4e2f64db82
Agustina <agustinaosanocorbo@gmail.com>
2025-04-08 19:49:34 -0300

    changed port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const recipeRoutes = require('./routes/recipeRoutes');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
